900f818b91033fe3099d6e6baa56e07a898deb2a
Leonard Schuetz <leni.schuetz@me.com>
2016-08-11 15:06:25 +0200

    Better comments

--- src/Events.cr
+++ src/Events.cr
@@ -1,57 +1,73 @@
 require "./Events/*"
 
-# This is a module
+# Adds the @__events instance variable to the current class
+module Events
+  @__events = {} of String => Event
+end
+
+# Adds the `Event` class
 module Events
 
+  # All saved events and their callback-procs are stored in this instance variable
   @__events = {} of String => Event
 
-  # Event class
-  # Keeps track of subscribers and invocations
+  # Event
+  #
+  # Saves all callbacks for a given event
+  # Also adds the functionality to invoke all methods
   class Event
-    getter name
     getter handlers
 
-    @name : String
     @handlers = [] of ->
 
-    # Create a new event, giving it a name
-    def initialize(@name)
-    end
-
-    # Adds a handler to the event
+    # Add a new handler
+    # Returns a proc that removes the handler from the event
     def add_handler(block)
       @handlers << block
+
+      ->{
+        @handlers.delete block
+      }
     end
 
-    # Invokes all currently registered handlers
+    # Removes a handler from the event
+    # You need to pass the exact same proc as you did for add_handler
+    # This just uses the equality check performed by Array#delete
+    def remove_handler(block)
+      @handlers.delete block
+    end
+
+    # Invokes all handlers
     def invoke
       @handlers.each do |handler|
         handler.call
       end
     end
   end
+end
 
-  # Add an event with a name
+# Adds public functions for adding, removing, invoking and listening events
+module Events
+
+  # Add an event with name *name*
   def add_event(name : String)
     @__events[name] = Event.new name
   end
 
-  # Removes an event by it's name
+  # Removes an event by *name*
   def remove_event(name : String)
     @__events.delete name
   end
 
-  # Invoke a given event
+  # Invoke a given event by *name*
   def invoke_event(name : String)
     if event_exists name
       @__events[name].invoke
     end
   end
 
-  # Add a handler to an event
+  # Add a handler to the event *name*
   # If the event doesn't exist, it will be created
-  #
-  # A handler should be a block receiving a string
   def on(name : String, &block : ->)
     if !event_exists name
       add_event name
@@ -60,7 +76,7 @@ module Events
     @__events[name].add_handler block
   end
 
-  # Checks if a given event exists
+  # Checks if a given event with *name* exists
   def event_exists(name : String)
     @__events.has_key? name
   end
